5388964303d048ef2325a70f0388e8916ba4550d
Vichakorn Yotboonrueang <vichacorn@outlook.com>
2021-01-31 02:44:18 +0700

    save data to sqlite

--- portfolio/views.py
+++ portfolio/views.py
@@ -2,10 +2,18 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 
+from .models import Contact
+
 def index(request):
     return render(request, 'index.html')
 
 @csrf_exempt
 def submit(request):
-    print(request.POST)
-    return HttpResponse("Your Submit Sucess!!")
+    if(request.method == "POST"):
+        contact = Contact()
+        contact.name = request.POST.get("name")
+        contact.email = request.POST.get("email")
+        contact.msg = request.POST.get("message")
+        contact.save()
+        return HttpResponse(status=200)
+    return HttpResponse(status=404)
