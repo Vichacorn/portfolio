d470dc5a2eef7c3976c2751a1a382cb73036031d
Vichakorn Yotboonrueang <vichacorn@outlook.com>
2021-01-31 02:23:43 +0700

    add post method

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,5 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 def index(request):
     return render(request, 'index.html')
+
+@csrf_exempt
+def submit(request):
+    print(request.POST)
+    return HttpResponse("Your Submit Sucess!!")
